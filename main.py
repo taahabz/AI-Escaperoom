766d3f2482713a05592c97e750863c5bfc39476f
taahabz <taahabz@gmail.com>
2025-01-09 04:40:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -351,8 +351,6 @@ has_key = False if collected_keys else True
 player_rect = pygame.Rect(
     current_level_data["start_pos"][0] * SCALED_TILE_SIZE,
     current_level_data["start_pos"][1] * SCALED_TILE_SIZE,
-    SCALED_TILE_SIZEc cx cxdscas    ,
-    SCALED_TILE_SIZE
 )
 camera_x = 0
 
